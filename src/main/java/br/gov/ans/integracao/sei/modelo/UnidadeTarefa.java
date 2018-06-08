417b7ae2795a983eec07fefadebd3e4e4a1e3627
andre.guimaraes <andre.guimaraes@ans.gov.br>
2018-06-08 11:57:04 -0300

    Omite atributo tarefa do objeto UnidadeTarefa
    Omissão do atributo tarefa que estava sendo retornado na consulta às
    unidades do processo.

--- src/main/java/br/gov/ans/integracao/sei/modelo/UnidadeTarefa.java
+++ src/main/java/br/gov/ans/integracao/sei/modelo/UnidadeTarefa.java
@@ -2,17 +2,22 @@ package br.gov.ans.integracao.sei.modelo;
 
 import javax.persistence.Entity;
 
+import org.codehaus.jackson.annotate.JsonIgnore;
+
 import br.gov.ans.integracao.sei.client.Unidade;
 
 @Entity
 public class UnidadeTarefa extends Unidade{
 	
+	@JsonIgnore
 	private int tarefa;
 
+	@JsonIgnore
 	public int getTarefa() {
 		return tarefa;
 	}
 
+	@JsonIgnore
 	public void setTarefa(int tarefa) {
 		this.tarefa = tarefa;
 	}
